b73f513b8dff703290bc4bf7713bf0083aa93b28
akulikoff <akulikoff86@gmail.com>
2017-10-12 08:56:33 +1000

    initial commit
     форма с валидацией через регулярные выражения к отображению ошибок добавлен эффект...	akulikoff	08:37

---
+++ js/script.js
@@ -0,0 +1,64 @@
+jQuery(document).ready(function($) {
+  var slider = $('#lightSlider').lightSlider({
+  	
+  	item:1,
+  	gallery:true,
+  	loop:true,
+  	thumbItem:10,
+  	vertical:false,
+  	
+  	onSliderLoad: function (el) {
+  		
+  		el.lightGallery({
+  			
+  			selector: '#lightSlider .lslide'
+  			
+  		})
+  		
+  	},
+  	//vThumbWidth:50,
+  	//verticalHeight:250,
+  	//thumbMargin:10
+  	
+  	//slideMove:1,
+  	//speed:100,
+  	//slideMargin:30,
+  	
+  	/*responsive: [
+  		
+  		{
+			breakpoint:800,
+			settings: {
+				item:3,
+				slideMove:2,
+				slideMargin:10
+			}
+		},
+		
+		{
+			breakpoint:400,
+			settings: {
+				item:2,
+				slideMove:1,
+				slideMargin:1
+			}
+		}
+  	
+  	],*/
+  	
+  	//autoWidth:true,
+  	//adaptiveHeight:true,
+  	//rtl:true,
+  	
+  }); 
+  
+  $('#button').click(function () {
+  	
+  	slider.goToPrevSlide();
+  	
+  	
+  });
+  
+   
+});
+
